f9dd279364f820fddad89e1d09830384e972bad6
Mathias Sund <mathias@mathiass-mbp.lan>
2019-10-03 12:32:08 +0200

    Removed some example code + comments

--- src/data/SwedishTrainAPI.js
+++ src/data/SwedishTrainAPI.js
@@ -43,12 +43,4 @@ export default class SwedishTrainAPI extends RESTDataSource {
     const result = await this.post(this.baseURL, body);
     return result.RESPONSE.RESULT[0].TrainAnnouncement;
   }
-
-  async getMostViewedMovies(limit = 10) {
-    const data = await this.get("movies", {
-      per_page: limit,
-      order_by: "most_viewed"
-    });
-    return data.results;
-  }
 }
--- src/schema/index.js
+++ src/schema/index.js
@@ -14,9 +14,6 @@ const schemas = gql`
     Canceled: Boolean
   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     trainsFromMalmo: [TrainDepartureSE]
   }
